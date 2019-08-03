0d4c8e02987c4fd0f179a1b4b5ae0667d2fa6b77
menglijinghua <508304482@qq.com>
2019-08-03 08:48:00 +0800

    ALL

---
+++ src/main/java/cn/blabla/provider/GithubProvider.java
@@ -0,0 +1,48 @@
+package cn.blabla.provider;
+
+import cn.blabla.dto.AccessTokenDto;
+import cn.blabla.dto.GithubUser;
+import com.alibaba.fastjson.JSON;
+import okhttp3.*;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+
+@Component
+public class GithubProvider {
+
+    public String getAccessToken(AccessTokenDto accessTokenDto) {
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDto));
+        Request request = new Request.Builder()
+                .url("https://github.com/login/oauth/authorize")
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String string = response.body().string();
+            String token = string.split("&")[0].split("=")[1];
+            return token;
+        } catch (Exception e) {
+
+        }
+        return null;
+    }
+
+    public GithubUser getUser(String accessToken){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token="+accessToken)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String string = response.body().string();
+            GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
+            return githubUser;
+        } catch (IOException e) {
+        }
+        return null;
+    }
+
+}
